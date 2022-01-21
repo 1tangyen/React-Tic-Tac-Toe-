1d716316457cfdd76828a4ffd208ce906d662c16
1tangyen <51384739+1tangyen@users.noreply.github.com>
2022-01-21 18:44:45 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Game from './components/Game';
+
+export default function App() {
+	return (
+		<div>
+			<Game />
+		</div>
+	);
+}
